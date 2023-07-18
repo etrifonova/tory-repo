227fac08ed20f92db192ca19f5ebda651f15bb4c
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-07-18 16:31:44 +0300

    feat: add new operations

--- main.js
+++ main.js
@@ -1,7 +1,61 @@
+const ELEMENT_1 = 5;
+const ELEMENT_2 = 7;
+
+// ADDITION (+)
+const SUM = ELEMENT_1 + ELEMENT_2;
+console.log(SUM); 
+// SUBTRACTION (-)
+const DIFFERENCE = ELEMENT_2 - ELEMENT_1;
+console.log(DIFFERENCE); 
+
+// MULTIPLICATION (*)
+const PRODUCT = ELEMENT_1 * ELEMENT_2;
+console.log(PRODUCT);
+// DIVISION (/)
+const QUOTIENT = PRODUCT / ELEMENT_1;
+console.log(QUOTIENT);
+
+// INCREMENTATION
+let inc = 10;
+inc++
+console.log(inc);
+
+
 // var a;
 // a = 7;
 // var b;
 // b = a; 
 
-var myFirstName = "Victoria"; 
-var myLastName = "Shafieva";  
+// var myFirstName = "Victoria"; 
+// var myLastName = "Shafieva";  
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
+
+// a = a +1;
+// b = b +5;
+// c = c + " String!";
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// Modify the existing declarations and assignments so their names use camelCase.
+// Do not create any new variables.
+
+// Variable declarations
+// let studlyCapVar;
+// let properCamelCase;
+// let titleCaseOver;
+
+// Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+// Update the code so it only uses the let keyword.
+
+// let catName = "Oliver";
+// let catName = "Meow!";
+// console.log(catName);
+
